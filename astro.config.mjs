8a01cc5b135cc11f6ccb9dbfacee04ce52af08d2
Roland Bewick <roland.bewick@gmail.com>
2026-05-31 19:07:46 +0700

    feat: new design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,9 @@
 // @ts-check
-
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://sovereignbrain.me',
 	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
 });
